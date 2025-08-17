d661d21ea8b4787b637d318b63d3a7ac59f3f88d
HuyPhamBE <huypd200604@gmail.com>
2025-08-17 09:26:52 +0700

    allow user to get package

--- Fortune/Fortune/Controllers/OrderController.cs
+++ Fortune/Fortune/Controllers/OrderController.cs
@@ -22,7 +22,7 @@ namespace Fortune.Controllers
             return Ok(orders);
         }
         [HttpGet("GetUserPurchasePackage/{userId}")]
-        [Authorize(Roles = "3")]
+        [Authorize(Roles = "3,2,1")]
         public async Task<IActionResult> GetUserPurchasePackage(Guid userId)
         {
             var packages = await orderService.GetUserPurchasePackageAsync(userId);
